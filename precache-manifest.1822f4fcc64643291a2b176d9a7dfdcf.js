fa2f1f1326e1ea764fd62514fe0a4bd2a23456f1
Vanessa <vkb247@gmail.com>
2020-04-08 17:21:25 -0300

    Updates

---
+++ precache-manifest.1822f4fcc64643291a2b176d9a7dfdcf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d57fc9879f2d0d57e841c87829b26b",
+    "url": "/bermudabeaches/index.html"
+  },
+  {
+    "revision": "db91ec50be27df0e199a",
+    "url": "/bermudabeaches/static/css/main.92589205.chunk.css"
+  },
+  {
+    "revision": "122f72042775f83a83a9",
+    "url": "/bermudabeaches/static/js/2.e2881a7f.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/bermudabeaches/static/js/2.e2881a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db91ec50be27df0e199a",
+    "url": "/bermudabeaches/static/js/main.1c2b754e.chunk.js"
+  },
+  {
+    "revision": "50357960915fd10708cc",
+    "url": "/bermudabeaches/static/js/runtime-main.f4737479.js"
+  },
+  {
+    "revision": "2c8442af7543d750ca455f75f6e7a221",
+    "url": "/bermudabeaches/static/media/Footage.2c8442af.mp4"
+  },
+  {
+    "revision": "021b0d6f145fe99ba11c2cc01e65b8ac",
+    "url": "/bermudabeaches/static/media/img1.021b0d6f.jpg"
+  },
+  {
+    "revision": "cc0e5a11879b28ae53b1990bda0e280d",
+    "url": "/bermudabeaches/static/media/img2.cc0e5a11.jpg"
+  },
+  {
+    "revision": "d604faf706cf717f1d8a57bcae485972",
+    "url": "/bermudabeaches/static/media/img3.d604faf7.jpg"
+  },
+  {
+    "revision": "95e5008d6ef482aaaa717c86c0168b6c",
+    "url": "/bermudabeaches/static/media/img4.95e5008d.jpg"
+  },
+  {
+    "revision": "b0858ca1cd989b0cd35e96ce6210125b",
+    "url": "/bermudabeaches/static/media/img5.b0858ca1.png"
+  },
+  {
+    "revision": "525eb64ae480a695a91db0eb2aba4d43",
+    "url": "/bermudabeaches/static/media/img6.525eb64a.jpg"
+  }
+]);
--- src/components/button/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./button.css";
-
-function Button({ title }) {
-  return <button className="btn btn-outline-light btn-lg">{title}</button>;
-}
-export default Button;
